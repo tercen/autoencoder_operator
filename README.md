e48ff43a177229b9d8067d7a874a747fb32b6624
agouy <contact@agouy.com>
2021-04-30 21:21:47 +0000

    draft

---
+++ README.md
@@ -0,0 +1,35 @@
+# Autoencoder operator
+
+##### Description
+
+The `Template R operator` is a template repository for the creation of R operators in Tercen.
+
+##### Usage
+
+Input projection|.
+---|---
+`x-axis`        | type, description 
+`y-axis`        | type, description 
+`row`           | type, description 
+`column`        | type, description 
+`colors`        | type, description 
+`labels`        | type, description 
+
+Input parameters|.
+---|---
+`input_var`        | parameter description
+
+Output relations|.
+---|---
+`output_var`        | output relation
+`Operator view`        | view of the Shiny application
+
+##### Details
+
+Details on the computation.
+
+##### See Also
+
+[template_shiny_operator](https://github.com/tercen/template_shiny_operator)
+, [template_docker_operator](https://github.com/tercen/template_docker_operator)
+
---
+++ workspace.R
@@ -0,0 +1,66 @@
+library(tercen)
+library(dplyr)
+library(ANN2)
+
+options("tercen.workflowId" = "e9482bf86b951d32f9ff44a499015119")
+options("tercen.stepId"     = "644f92ec-8807-435e-b6b5-d6045440913c")
+
+minmax <- function(x) { return((x- min(x)) /(max(x)-min(x))) }
+
+ctx <- tercenCtx()
+df <- ctx$as.matrix() %>% t %>% as_tibble()
+df <- as_tibble(lapply(df, minmax))
+colnames(df) <- ctx$rselect()[[1]]
+
+hidden_layers <- ifelse(
+  !is.null(ctx$op.value('hidden_layers')),
+  as.character(ctx$op.value('hidden_layers')),
+  "32, 2, 32"
+)
+hidden_layers <- as.numeric(trimws(strsplit(hidden_layers, ",")[[1]]))
+
+activ_functions <- ifelse(
+  !is.null(ctx$op.value('activ_functions')),
+  as.character(ctx$op.value('activ_functions')),
+  "relu, linear, relu"
+)
+
+optim_type <- ifelse(
+  !is.null(ctx$op.value('optim_type')),
+  as.character(ctx$op.value('optim_type')),
+  "adam"
+)
+
+loss_type <- ifelse(
+  !is.null(ctx$op.value('loss_type')),
+  as.character(ctx$op.value('loss_type')),
+  "squared"
+)
+
+n_epochs <- ifelse(
+  !is.null(ctx$op.value('n_epochs')),
+  as.numeric(ctx$op.value('n_epochs')),
+  30
+)
+
+ac <- autoencoder(
+  df,
+  hidden.layers = hidden_layers,
+  standardize = TRUE,
+  activ.functions = activ_functions,
+  optim.type = optim_type,
+  loss.type = loss_type,
+  n.epochs = n_epochs
+)
+
+encoded <- encode(ac, df)
+clusters <- max.col(encoded, ties.method = "first")
+
+df_out <- list(
+  encoded,
+  clusters = clusters,
+  .ci = 0:(length(clusters) - 1)
+) %>% 
+  as.data.frame() %>%
+  ctx$addNamespace() %>%
+  ctx$save()
